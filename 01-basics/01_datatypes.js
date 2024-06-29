3a98a33b4a650bb8a1d7fa7a14692ec342fa46d3
Yuvraj Singh Chowdhary <149966983+chowdhary19@users.noreply.github.com>
2024-06-29 13:00:11 +0000

    Commit

---
+++ 01-basics/01_datatypes.js
@@ -0,0 +1,17 @@
+"use strict" ; //sets the theme of th file to newer version
+
+
+// alert(5+5) //using nodejes, hence cannot directly use alert, not in browser
+
+// number
+// bigint
+// string
+// boolean
+// null
+// undefined
+// These all are primitve datatypes
+
+// we have object datatypes as well
+
+console.log(typeof null)
+
---
+++ 01-basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 234124
+let accountEmail="chowdhary@gmail.com"
+var accountPassword = "123456"
+accountCity="Lucknow"
+
+// Variables can be defined without let or var or const, but thet are of global scope, and we generally dont use them.
+
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
